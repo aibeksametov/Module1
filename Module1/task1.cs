3c68b0a610011cfc61a2559e9b7c11f7c67564ab
samet <samet@DESKTOP-OEAKA56>
2023-10-22 00:51:42 +0600

    Add project files.

---
+++ Module1/task1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class task1
+{
+    public static void Main(string[] args)
+    {
+
+        Console.Write("Введите  ФИО: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Приветствую тебя - " + name);
+
+        Console.ReadLine();
+    }
+}
